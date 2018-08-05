4c0862c46af7c665ad9072b5af49d5eff9a45578
Steven Huang <steven.charles.huang@gmail.com>
2018-08-05 00:08:16 -0400

    added constructor to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div className="App">
