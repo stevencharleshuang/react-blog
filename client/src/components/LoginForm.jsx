1ac57fddc0102164e64b602ff22c151af7759232
Steven Huang <steven.charles.huang@gmail.com>
2018-08-22 23:33:41 -0400

    added event listeners to form and submit button

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -9,23 +9,36 @@ export default class LoginForm extends React.Component {
     }
     this.handleUsername = this.handleUsername.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleUsername() {
-
+  handleUsername(e) {
+    // console.log(e.target.value);
+    this.setState({
+      username: e.target.value
+    });
   }
 
-  handlePassword() {
+  handlePassword(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
 
+  handleClick(e) {
+    e.preventDefault();
   }
 
   render() {
+    console.log('state: ', this.state)
     return (
     <div className="login-form">
       <form>
         <input type="text" id="username" name="username" placeholder="Username" onChange={this.handleUsername} />
         <br />
         <input type="text" id="password" name="password" placeholder="Password" onChange={this.handlePassword} />
+        <br />
+        <button onClick={this.handleClick}>Login</button>
       </form>
     </div>
     );
