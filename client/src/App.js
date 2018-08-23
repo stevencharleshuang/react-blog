de65821044b87e2edcd88c9b08391aa9108b6c41
Steven Huang <steven.charles.huang@gmail.com>
2018-08-23 16:47:49 -0400

    added logout feature

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: ''
+      authenticated: false,
     }
   }
 
@@ -19,7 +19,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Welcome to React Blog, Dave</h1>
-        <LoginForm />
+        <LoginForm authenticated={this.state.authenticated} />
       </div>
     );
   }
--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -8,11 +8,12 @@ export default class LoginForm extends React.Component {
     this.state = {
       username: '',
       password: '',
-      token: undefined,
+      authenticated: this.props.authenticated
     }
     this.handleUsername = this.handleUsername.bind(this);
     this.handlePassword = this.handlePassword.bind(this);
-    this.handleClick    = this.handleClick.bind(this);
+    this.handleLogIn    = this.handleLogIn.bind(this);
+    this.handleLogOut   = this.handleLogOut.bind(this);
   }
 
   handleUsername(e) {
@@ -28,9 +29,14 @@ export default class LoginForm extends React.Component {
     });
   }
 
-  handleClick(e) {
+  handleLogIn(e) {
     e.preventDefault();
     this.handleSubmit(this.state)
+  }
+
+  handleLogOut(e) {
+    e.preventDefault();
+    TokenService.destroy();
   }
 
   handleSubmit(formData) {
@@ -46,15 +52,18 @@ export default class LoginForm extends React.Component {
       .then(res => res.json())
       .then(response => {
         console.log('Success:', (response))
-        TokenService.save(response.token)
+        TokenService.save(response.token);
+        this.setState({ authenticated: true })
       })
       .catch(error => console.error('Error:', error));
   }
 
   render() {
-    console.log('state: ', this.state)
+    // console.log('state: ', this.state)
     return (
     <div className="login-form">
+      <span>Authenticated: {this.state.authenticated ? 'True' : 'False'}</span>
+      <br />
       <form>
         <input
           type="text"
@@ -72,7 +81,8 @@ export default class LoginForm extends React.Component {
           onChange={this.handlePassword}
         />
         <br />
-        <button onClick={this.handleClick}>Login</button>
+        <button onClick={this.handleLogIn} data-id="login">Log In</button>
+        <button onClick={this.handleLogOut} data-id="logout">Log Out</button>
       </form>
     </div>
     );
