13ab49ffcda5e47a35c54f293c520cf2c631d989
Steven Huang <steven.charles.huang@gmail.com>
2018-08-30 00:01:35 -0400

    refactored route order for usersRouter for best practices, entries route pulling getAll entries successfully

--- server/routes/entriesRouter.js
+++ server/routes/entriesRouter.js
@@ -2,9 +2,12 @@ const entriesRouter             = require('express').Router();
 const entriesController         = require('../controllers/usersController');
 const entriesResponseController = require('../controllers/usersResponseController');
 
-entriesRouter.get('/', () => {
-  res.send({ message: 'You hit the entries router, Dave' })
-});
+// entriesRouter.get('/', (req, res, next) => {
+//   res.send({ message: 'You hit the entries router, Dave' })
+// });
 
+entriesRouter.route('/')
+  .get(entriesController.getAll, entriesResponseController.sendJSON)
+  .post(entriesController.registerUser, entriesResponseController.handleCreateUser)
 
 module.exports = entriesRouter;
