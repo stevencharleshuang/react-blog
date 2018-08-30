65f90803e69df7531cb9d0eecfa375401a9c8922
Steven Huang <steven.charles.huang@gmail.com>
2018-08-29 23:17:31 -0400

    refactored db schema and seeds for dummy blog entries

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,11 +3,20 @@ CREATE DATABASE react_blog;
 
 \c react_blog
 
-CREATE TABLE users(
-   id     SERIAL PRIMARY KEY NOT NULL
-  ,name   VARCHAR(50) NOT NULL
-  ,username VARCHAR(50) NOT NULL
-  ,email  VARCHAR(50) NOT NULL
-  ,password VARCHAR(100) NOT NULL
-  ,avatar_url VARCHAR(255)
+CREATE TABLE users (
+  id         SERIAL PRIMARY KEY NOT NULL,
+  name       VARCHAR(50) NOT NULL,
+  username   VARCHAR(50) NOT NULL,
+  email      VARCHAR(50) NOT NULL,
+  password   VARCHAR(100) NOT NULL,
+  avatar_url VARCHAR(255)
+);
+
+CREATE TABLE entries (
+  id           SERIAL PRIMARY KEY NOT NULL,
+  date_created INTEGER NOT NULL,
+  location     VARCHAR(255) NOT NULL,
+  title        VARCHAR (500) NOT NULL,
+  content      TEXT NOT NULL,
+  user_id      INTEGER NOT NULL
 );
