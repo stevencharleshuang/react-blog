98317b48a4a995d004b29e7758e01f0e44e264f5
Steven Huang <steven.charles.huang@gmail.com>
2018-09-03 01:33:02 -0400

    UserProfile successfully checking for token and rendering conditionally, need to debug flow jumping for props not being passed

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Nav (props) {
+  console.log(props);
   return (
     <nav>
       <ul>
         <Link to="/"><li>Home</li></Link>
-        <Link to="/user/:username"><li>User Private Profile</li></Link>
+        <Link to="/user/:id"><li>User Private Profile</li></Link>
         <Link to="/login"><li>Log In</li></Link>
         <Link to="/register"><li>Register</li></Link>
       </ul>
--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -2,21 +2,37 @@ import React from 'react';
 import Entries from './Entries';
 import EditUserForm from './EditUserForm';
 import CreateEntryForm from './CreateEntryForm';
+import TokenService from '../services/TokenService';
 
 export default class UserProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       user: this.props.location.state.user,
+      authenticated: false,
+    }
+  }
+
+  componentWillMount() {
+    if (TokenService) {
+      this.setState((prevState) => ({
+        authenticated: !prevState.authenticated
+      }))
     }
   }
 
   render() {
     console.log('UserProfile state: ', this.state);
     console.log('UserProfile props: ', this.props);
+    const user = this.state.user;
     return(
       <div className="user-profile">
-        <h1>Hello, {this.state.user.username}!</h1>
+        <h1>Hello, {user.username}!</h1>
+        {
+          !this.state.authenticated
+            ? 'You get the public view'
+            : 'You get the private view'
+        }
       </div>
     );
   }
